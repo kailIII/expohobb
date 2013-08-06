a148716f63bc6e75370c89abdd41dba44b440ce1
Marcelo Rusconi <mgrusconi@gmail.com>
2013-08-05 21:25:14 -0300

    test revista

--- ver_revista.php
+++ ver_revista.php
@@ -90,31 +90,16 @@
                   <?php echo $revista['description'];?>
                 </div>
                   <div class="cont-arch">
-                  	<object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="580" height="456">
-                  	  <param name="movie" value="<?php echo $revista['swf']; ?>">
-                  	  <param name="quality" value="high">
-                  	  <param name="wmode" value="transparent">
-                  	  <param name="swfversion" value="6.0.65.0">
-                  	  <param name="expressinstall" value="Scripts/expressInstall.swf">
-                  	  <param name="SCALE" value="noborder">
-                  	  <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
-                  	  <!--[if !IE]>-->
-                  	  <object type="application/x-shockwave-flash" data="<?php echo $revista['swf']; ?>" width="580" height="456">
-                  	    <!--<![endif]-->
-                  	    <param name="quality" value="high">
-                  	    <param name="wmode" value="transparent">
-                  	    <param name="swfversion" value="6.0.65.0">
-                  	    <param name="expressinstall" value="Scripts/expressInstall.swf">
-                  	    <param name="SCALE" value="noborder">
-                  	    <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
-                  	    <div>
-                  	      <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
-                  	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
-                	      </div>
-                  	    <!--[if !IE]>-->
-                	    </object>
-                  	  <!--<![endif]-->
-                	  </object>
+                  	<iframe 
+                      name="Joomag_embed_56b6d485-04ea-4288-b1f6-c3769ceb55e7"
+                      style="width:750px;height:500px" 
+                      width="750px" 
+                      height="500px" 
+                      hspace="0" 
+                      vspace="0"
+                      frameborder="0"  
+                      src="http://www.joomag.com/magazine/expohobby-test-1-1/0895520001375518971?p=1&e=1&embedInfo=;image,http://www.joomag.com/Frontend/flash/gui/themes/default_new/thumbnail.png,fill">
+                  </iframe>
                   </div>
                   <div class="cont-btn-pdf">
                    <a class="btn-classic" href="<?php echo $revista['pdf'];?>" target="_blank" title="Descargar <?php echo $revista['title'];?> en formato PDF"> Descargar <?php echo $revista['title'];?> en formato PDF</a>
